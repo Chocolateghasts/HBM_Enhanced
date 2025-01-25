e2215a6fba640752d6854b42790da5ea3800570e
omlet <maximgasparotto120@gmail.com>
2025-01-25 16:46:20 +0100

    Add files via upload

--- src/main/java/com/mewo/hbmenhanced/LabBlock.java
+++ src/main/java/com/mewo/hbmenhanced/LabBlock.java
@@ -1,7 +1,10 @@
 package com.mewo.hbmenhanced;
 
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IIconRegister;
 import net.minecraft.creativetab.CreativeTabs;
 
 public class LabBlock extends Block {
@@ -13,6 +16,11 @@ public class LabBlock extends Block {
         this.setResistance(1.0F);
         this.setStepSound(soundTypeWood);
         this.setCreativeTab(CreativeTabs.tabBlock);
+    }
+
+    @SideOnly(Side.CLIENT)
+    public void registerBlockIcons(IIconRegister register) {
+        this.blockIcon = register.registerIcon(hbmenhanced.MODID + ":" + this.getUnlocalizedName().substring(5));
     }
 
 }
