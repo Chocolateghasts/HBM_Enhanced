cef0b3133fce759a4ead9bdd221baf5794902719
ChocolateMods <chocolateghasts9@gmail.com>
2025-01-25 01:23:55 +0100

    Test git relink

--- src/main/java/com/mewo/hbmenhanced/hbmenhanced.java
+++ src/main/java/com/mewo/hbmenhanced/hbmenhanced.java
@@ -2,11 +2,16 @@ package com.mewo.hbmenhanced;
 
 import com.mewo.hbmenhanced.commands.RPCommand;
 import com.mewo.hbmenhanced.commands.showRPCommand;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import net.minecraft.init.Blocks;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+import java.io.IOException;
+import java.util.Timer;
+import java.util.TimerTask;
 
 @Mod(modid = hbmenhanced.MODID, version = hbmenhanced.VERSION)
 public class hbmenhanced
@@ -21,9 +26,26 @@ public class hbmenhanced
         System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
     }
     @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        saveRPData.loadRPData();
+    }
+
+    @EventHandler
     public void serverStarting(FMLServerStartingEvent event) {
         // Register your custom command
         event.registerServerCommand(new RPCommand());
         event.registerServerCommand(new showRPCommand());
+
+        Timer timer = new Timer(true);
+        timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    saveRPData.saveRPData();  // Periodically save RP data
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, 0, 60000);  // Save every 60 seconds
     }
 }
