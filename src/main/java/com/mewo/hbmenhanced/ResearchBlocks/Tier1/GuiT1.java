bbb21e1789fe7ffde4a793b7047f517b1d33bfb7
ChocolateMods <152035797+Chocolateghasts@users.noreply.github.com>
2025-07-09 15:02:39 +0200

    Revamping LabBlock

--- src/main/java/com/mewo/hbmenhanced/ResearchBlocks/Tier1/GuiT1.java
+++ src/main/java/com/mewo/hbmenhanced/ResearchBlocks/Tier1/GuiT1.java
@@ -3,6 +3,7 @@ package com.mewo.hbmenhanced.ResearchBlocks.Tier1;
 import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
 
 public class GuiT1 extends GuiContainer {
 
@@ -20,7 +21,11 @@ public class GuiT1 extends GuiContainer {
 
     @Override
     protected void drawGuiContainerBackgroundLayer(float p_146976_1_, int p_146976_2_, int p_146976_3_) {
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         this.mc.getTextureManager().bindTexture(texture);
+        int k = (this.width - this.xSize) / 2;
+        int l = (this.height - this.ySize) / 2;
+        this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
     }
 
     @Override
