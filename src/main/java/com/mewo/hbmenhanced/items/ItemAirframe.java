3c868028c96bb45a741d7f10d65c651aa5f6aee4
JanAppeltaart <Svendingerdis20@outlook.com>
2025-08-06 14:46:42 +0200

    Added the pong game to the labblock

---
+++ src/main/java/com/mewo/hbmenhanced/items/ItemAirframe.java
@@ -0,0 +1,11 @@
+package com.mewo.hbmenhanced.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemAirframe extends Item {
+    public ItemAirframe() {
+        setUnlocalizedName("Airframe");
+        setCreativeTab(CreativeTabs.tabMisc);
+    }
+}
